cfd1ac0fa73e4752ef485be0d659cab524daaa39
Opilane TTHK <opilane@tthk.ee>
2021-09-09 10:28:46 +0300

    ControlFlow

---
+++ ControlFlow/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ControlFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // programm küsib kasutaja vanust
+            // kui kasutaja on vanem kui 18, siis konsoolis kuvatakse "Oled piisavalt vana"
+            // kui kasutaja on noorem kui 18, siis konsoolis kuvatakse "Oled liiga noor"
+            //kui kasutaja  on 18, siis konsoolis kuvatakse "oledki 18"
+
+            Console.WriteLine("Sisestage oma vanus");
+            int Age = Convert.ToInt32(Console.ReadLine());
+
+             if (Age > 18)
+            {
+              Console.WriteLine("Oled piisavalt vana.");
+            }
+             else if (Age < 18)
+            {
+                Console.WriteLine("Oled liiga noor.");
+            }
+             else
+            {
+                Console.WriteLine("Oledki 18.");
+            }
+            Console.WriteLine("Kena päeva inimene");
+        }
+    }
+}
---
+++ DrivingLisence/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DrivingLisence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaatat
+            //programm kontrollib kasutaja vanust
+            //programm teatab kasutjad, kas ta on piisavalt vana,
+            //et juhiluba saada (tingimuseks 18 aastat vana)
+
+            Console.WriteLine("Sisestage oma sünniaasta");
+            
+            int YearOfBirth = Convert.ToInt32(Console.ReadLine());
+
+            int Age = 2021 - YearOfBirth;
+
+            if (Age >= 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et teha juhilube.");
+            }
+            else 
+            {
+                Console.WriteLine("Oled liiga noor,tingimuseks on 18+.");
+            }
+            
+            Console.WriteLine("Kena päeva.");
+            
+
+        }
+    }
+}
